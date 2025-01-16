4c8d6c186835215dc7113a881f8e2ed7fd76cffd
LuigiRac <lui.rac06@gmail.com>
2025-01-16 23:55:42 +0100

    inserita navbar e importato NavLink

--- src/components/pages/HeaderContent.jsx
+++ src/components/pages/HeaderContent.jsx
@@ -1,9 +1,28 @@
+import { Link, NavLink } from "react-router-dom";
+
 function HeaderContent() {
-    const title = "Header";
 
     return (
         <header>
-            <div className="title">{title}</div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand">Navbar</a>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <NavLink to="/HomePage">Home Page</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/About Us">About Us</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/PostList">Post List</NavLink>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+
         </header >
     )
 };
